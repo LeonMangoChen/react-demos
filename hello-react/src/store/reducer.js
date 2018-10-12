360067b4f6a3b05117f7de93bd6c1948157db718
LeonMangoChen <leonmangochen@163.com>
2018-10-12 08:36:25 +0800

    Add deep copy function

--- hello-react/src/store/reducer.js
+++ hello-react/src/store/reducer.js
@@ -8,20 +8,20 @@ const defaultState = {
 // reducer 可以接收state，但是绝不能修改state
 export default (state = defaultState, action) => {
 	if (action.type === CHANGE_INPUT_VALUE) {
-		const newState = state;
+		const newState = JSON.parse(JSON.stringify(state));
 		newState.inputValue = action.value;
 		return newState;
 	}
 	if (action.type === ADD_ITEM) {
-		const newState = state;
-		if (newState.inputValue !='' ) {
+		const newState = JSON.parse(JSON.stringify(state));
+		if (newState.inputValue !== '' ) {
 			newState.list.push(newState.inputValue);
 			newState.inputValue='';
 		}
 		return newState;
 	}
 	if (action.type === DELETE_ITEM) {
-		const newState = state;
+		const newState = JSON.parse(JSON.stringify(state));
 		newState.list.splice(action.index,1);
 		return newState;
 	}
